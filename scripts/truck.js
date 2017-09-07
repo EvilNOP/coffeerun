2c245ff856a84a78e300a646e2a43bf89d64dcc3
Matthew <evilnops@gmail.com>
2017-09-07 21:56:07 +0800

    Fixed the owner of callback by calling `bind` method

--- scripts/truck.js
+++ scripts/truck.js
@@ -24,7 +24,7 @@
     console.log('Truck #' + this.truckId + ' has pending orders:');
     customerIdArray.forEach(function (id) {
       console.log(this.db.get(id));
-    });
+    }.bind(this));
   };
   
   App.Truck = Truck;
