ec5fd383559b66fa4a40fa4e5b6341e890fff493
Matthew <evilnops@gmail.com>
2017-09-07 12:02:18 +0800

    Added `add get getAll remove` methods to `DataStore` constructor

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -7,6 +7,22 @@
     this.data = {};
   }
   
+  DataStore.prototype.add = function (key, val) {
+    this.data[key] = val;
+  };
+  
+  DataStore.prototype.get = function (key) {
+    return this.data[key];
+  };
+  
+  DataStore.prototype.getAll = function () {
+    return this.data;
+  };
+  
+  DataStore.prototype.remove = function (key) {
+    delete this.data[key];
+  };
+  
   App.DataStore = DataStore;
   window.App = App;
 })(window);
