d89e406d9fb1023a4c17f5a4f8ffdc8ba48519a2
Matthew <evilnops@gmail.com>
2017-09-12 13:21:02 +0800

    Associated the validation check with the input event

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,7 @@
   var Truck = App.Truck;
   var DataStore = App.DataStore;
   var FormHandler = App.FormHandler;
+  var Validation = App.Validation;
   var CheckList = App.CheckList;
   
   var myTruck = new Truck('ncc-1701', new DataStore());
@@ -20,6 +21,8 @@
     myTruck.createOrder.call(myTruck, data);
     checklist.addRow.call(checklist, data);
   });
+  
+  formHandler.addInputHandler(Validation.isCompanyEmail);
   
   window.myTruck = myTruck;
 }(window));
