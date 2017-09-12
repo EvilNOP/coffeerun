47169345a0266ae004d9455a83e5cca5959652ca
Matthew <evilnops@gmail.com>
2017-09-12 11:52:50 +0800

    Listened for input event

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -38,6 +38,16 @@
     });
   };
   
+  FormHandler.prototype.addInputHandler = function (fn) {
+    console.log('Setting input handler for form');
+    
+    this.$formElement.on('input', '[name="emailAddress"]', function (event) {
+      var emailAddress = event.target.value;
+      
+      console.log(fn(emailAddress));
+    })
+  }
+  
   App.FormHandler = FormHandler;
   window.App = App;
 })(window);
