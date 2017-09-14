a45854788dcebbdb789edddc37066d1a336f2b39
Matthew <evilnops@gmail.com>
2017-09-14 22:32:00 +0800

    Created `get` method

--- scripts/remotedatastore.js
+++ scripts/remotedatastore.js
@@ -26,6 +26,14 @@
     });
   };
   
+  RemoteDataStore.prototype.get = function (key, cb) {
+    $.get(this.serverUrl + '/' + key, function (serverResponse) {
+      console.log(serverResponse);
+      
+      cb(serverResponse);
+    });
+  };
+  
   App.RemoteDataStore = RemoteDataStore;
   window.App = App;
 })(window);
