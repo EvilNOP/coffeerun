5c51dd013f72ee805482f2f4af8c3825fbb39a0b
Matthew <evilnops@gmail.com>
2017-09-14 20:01:38 +0800

    Created `add` method using jQuery's $.post method

--- scripts/remotedatastore.js
+++ scripts/remotedatastore.js
@@ -12,6 +12,10 @@
     this.serverUrl = url;
   }
   
+  RemoteDataStore.prototype.add = function (key, val) {
+    $.post(this.serverUrl, val);
+  };
+  
   App.RemoteDataStore = RemoteDataStore;
   window.App = App;
 })(window);
